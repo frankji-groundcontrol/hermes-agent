b46c8e9916665807b6738ad5183fbdb8181729c0
Frank Ji <dingjue.ji@yale.edu>
2026-08-12 06:06:39 +0000

    test: stabilize pooled state and event polling checks

--- tests/test_hermes_state.py
+++ tests/test_hermes_state.py
@@ -702,10 +702,10 @@ class TestFTS5Search:
         db.append_message("s1", role="user", content="after")
 
         statements = []
-        read_conn = db._get_read_conn() or db._conn
         traced_connections = [db._conn]
-        if read_conn is not db._conn:
-            traced_connections.append(read_conn)
+        with db._read_ctx() as read_conn:
+            if read_conn is not db._conn:
+                traced_connections.append(read_conn)
         for conn in traced_connections:
             conn.set_trace_callback(statements.append)
 
